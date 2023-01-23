622fedb842d1e2000289c3bc082c3db1945eb990
renzgabrinao <50391561+renzgabrinao@users.noreply.github.com>
2023-01-23 11:53:47 -0800

    initial files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var adminUserName = _configuration["adminLogin:Username"];
+            var adminPassword = _configuration["AdminLogin:Password"];
+            var connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
